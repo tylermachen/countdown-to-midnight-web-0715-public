0cd8e38513cf94aff94f895c501f109c85413762
Tyler Machen <tylermachen@gmail.com>
2015-07-30 14:15:05 -0400

    create countdown methods

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+# fast countdown
+def countdown(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
 
-def countdown
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+# suspensful countdown
+def countdown_with_sleep(i)
+  while i > 0
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
